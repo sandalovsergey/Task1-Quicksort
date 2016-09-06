74acc8fab69c27c07982255be47478341d17f21e
sandalovsergey <serezha_sandalov@mail.ru>
2016-09-06 21:10:44 +0300

    first class

--- Task1-Quicksort/Task1/src/com/company/Javamethod.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-import java.util.Random;
-/**
- * Created by Sergey Sandalov on 04.09.2016.
- */
-public class Javamethod {
-
-
-}
--- Task1-Quicksort/Task1/src/com/company/method.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * Created by Sergey Sandalov on 04.09.2016.
- */
-public class  method{
-}
